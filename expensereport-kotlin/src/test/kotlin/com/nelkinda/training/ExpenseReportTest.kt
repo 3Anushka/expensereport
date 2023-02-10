551935175d5fe390f267d34248c71f8d14591a5b
3Anushka <anushkaj@sahaj.ai>
2023-02-10 12:30:05 +0530

    Add test for expense type as dinner

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -22,11 +22,31 @@ class ExpenseReportTest {
     }
 
     @Test
-    fun `should print report with zero values if empty list is passed`() {
+    fun `it should print report with zero values if empty list is passed`() {
 
         expenseReport.printReport(emptyList())
 
         assertEquals("Expenses ${Date()}\nMeal expenses: 0\nTotal expenses: 0\n", outputStreamCaptor.toString())
+    }
+
+    @Test
+    fun `it should print report when one dinner expense is passed`() {
+
+        val expense1 = Expense(ExpenseType.DINNER, 10)
+        val allExpenses = arrayListOf<Expense>()
+        allExpenses.add(expense1)
+
+
+        expenseReport.printReport(allExpenses)
+
+        val expectedOutput = "Expenses ${Date()}\nDinner\t10\t \nMeal expenses: 10\nTotal expenses: 10\n"
+        val actualOutput = outputStreamCaptor.toString()
+
+        checkAssertion(expectedOutput, actualOutput)
+    }
+
+    private fun checkAssertion(expectedOutput: String, actualOutput: String) {
+        assertEquals(expectedOutput, actualOutput)
     }
 
 
